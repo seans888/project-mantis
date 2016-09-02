38d44884cdf973c0a77cf1fad8623b453a88be05
Justine Siccion <justine.siccion@yahoo.com>
2016-09-02 17:38:17 +0800

    department backend

---
+++ Application/advanced/backend/views/department/_form.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Department */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="department-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+ 	 <?= $form->field($model, 'Dep_id')->textInput() ?>
+
+    <?= $form->field($model, 'Department_Name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'Description')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
